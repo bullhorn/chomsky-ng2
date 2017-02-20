79c6ac5ed06f0e056abe468ded9fe383e7f64a78
Joshua Godi <jgodi@bullhorn.com>
2017-02-19 21:54:36 -0600

    chore(code): Clean up compiled code

---
+++ src/services/translate.service.spec.ts
@@ -0,0 +1,15 @@
+// import { TranslateService } from './translate.service';
+//
+// describe('Service: TranslateService', () => {
+//     let service;
+//
+//     beforeEach(() => {
+//         service = new TranslateService();
+//     });
+//
+//     describe('TODO', () => {
+//         it('should return true', () => {
+//             expect(service).toBeDefined();
+//         });
+//     });
+// });
